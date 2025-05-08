9ad8c1f01dc17ef99e275f6a31494dcd069655ad
Darilton Mario <dariltonmario14@gmail.com>
2025-05-08 15:21:05 +0100

    Implement routing

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from "./pages/home/home.component";
-import { LoginComponent } from "./pages/login/login.component";
-import { SignupComponent } from "./pages/signup/signup.component";
-import { FooterComponent } from "./footer/footer.component";
-import { NavbarComponent } from "./navbar/navbar.component";
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { HomeComponent } from "./components/home/home.component";
+import { LoginComponent } from "./components/login/login.component";
+import { SignupComponent } from "./components/footer/signup/signup.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent, LoginComponent, SignupComponent, FooterComponent, NavbarComponent],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, HomeComponent, LoginComponent, SignupComponent, FooterComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/footer/signup/signup.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'home', component: HomeComponent},
+    {path:'login', component: LoginComponent},
+    {path: 'signup', component: SignupComponent},
+    {path: '',   redirectTo: '/home', pathMatch: 'full'}
+];
