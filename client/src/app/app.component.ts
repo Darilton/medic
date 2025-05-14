23cffe7ce8240bf972148e3d57868d91edb6d545
Darilton Mario <20211852@isptec.co.ao>
2025-05-14 16:11:41 +0100

    Remove unnecessary imports from app.component and app.routes

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
-import { HomeComponent } from "./components/home/home.component";
-import { LoginComponent } from "./components/login/login.component";
-import { SignupComponent } from "./components/footer/signup/signup.component";
+import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from "./components/footer/footer.component";
 import { NavbarComponent } from "./components/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, HomeComponent, LoginComponent, SignupComponent, FooterComponent, NavbarComponent],
+  imports: [RouterOutlet, FooterComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
-import { SignupComponent } from './components/footer/signup/signup.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { AboutComponent } from './components/about/about.component';
 
 export const routes: Routes = [
     {path:'home', component: HomeComponent},
     {path:'login', component: LoginComponent},
     {path: 'signup', component: SignupComponent},
+    {path: 'about', component: AboutComponent},
     {path: '',   redirectTo: '/home', pathMatch: 'full'}
 ];
