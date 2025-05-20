6e2dfec509d87bfdb1ad481b6e19f6618106128d
gumball <dariltonmario14@gmail.com>
2025-05-20 16:10:32 +0100

    Hide database connectionString with dotnet user-secrets

--- server/api/Program.cs
+++ server/api/Program.cs
@@ -3,14 +3,14 @@ using Microsoft.EntityFrameworkCore;
 using MySql.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-
+var connectionString = builder.Configuration["ConectionString"];
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<MedicContext>(options => options.UseMySQL(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<MedicContext>(options => options.UseMySQL(connectionString));
 
 var app = builder.Build();
 
